fc740affa11bdce61d260cff522267e1d4f2ab02
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-05-28 18:24:54 +0200

    add secondController

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,8 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,49 +11,53 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.Window;
 import java.io.IOException;
 
 
 public class Controller {
     public TextField logInText;
     public PasswordField passwordText;
-
     public Button singupButton;
     public Button loginButton;
     public Button logoutButton;
-
-
+    public Label instructionText;
 
     private static ServerCommunication serverCommunication = new ServerCommunication();
-
-    @FXML
-    public Label instructionText;
-    public Label testLabel;
-    @FXML
-    private Label nameLabel;
-    @FXML
-    private Label consoleText;
-
 
     public void singUpButton(ActionEvent actionEvent) {
     }
 
-
     public void loginMethod(ActionEvent actionEvent) throws IOException {
         serverCommunication.login(logInText.getText(), passwordText.getText());
 
-        System.out.println((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
-        instructionText.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
-        //testLabel.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
-        //consoleText.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
-        //nameLabel.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
 
 
-        Stage window = (Stage) loginButton.getScene().getWindow();
-        window.close();
 
 
+
+
+
+        //Load second scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("secondSample.fxml"));
+        Parent root = loader.load();
+
+        //Get controller of scene2
+        secondController scene2Controller = loader.getController();
+        //Pass whatever data you want. You can have multiple method calls here
+        secondController.setServerCommunication(serverCommunication);
+
+        //Show scene 2 in new window
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 1000, 500));
+        stage.setTitle("Second Window");
+        stage.show();
+
+
+
+
+
+
+/*
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader();
         Pane root = fxmlLoader.load(getClass().getResource("secondSample.fxml").openStream());
@@ -65,6 +67,18 @@ public class Controller {
         //Sfxml sfxml=fxmlLoader.getController();
         //sfxml.initData(stage);
         stage.show();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/javafxcontrollercommunication/scene2/scene2.fxml"));
+        Parent root = loader.load();
+        secondController scene2Controller = loader.getController();
+        //Pass whatever data you want. You can have multiple method calls here
+        scene2Controller.transferMessage(inputField.getText());
+
+
+*/
+
+        Stage window = (Stage) loginButton.getScene().getWindow();
+        window.close();
     }
 
     public void singupMethod(ActionEvent actionEvent) {
--- src/sample/secondController.java
+++ src/sample/secondController.java
@@ -1,15 +1,31 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class secondController {
     public TextField logInText;
-    public Label testLabel;
+    public Label consoleText;
+    public Button logoutButton;
+    public PasswordField passwordText;
+    public Label nameLabel;
+
+    private static ServerCommunication serverCommunication=null;
+
+    public static void setServerCommunication(ServerCommunication serverCommunication) {
+        secondController.serverCommunication = serverCommunication;
+    }
 
     public void logoutMethod(ActionEvent actionEvent) {
-        testLabel.setText("fkdjsfl;kj");
+        System.out.println((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
+        nameLabel.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
+        //testLabel.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
+        //consoleText.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
+        //nameLabel.setText((serverCommunication.getLogin() + " - " + serverCommunication.getFname() + " " + serverCommunication.getLname()).toUpperCase());
+
     }
 
     public void sendButton(ActionEvent actionEvent) {
