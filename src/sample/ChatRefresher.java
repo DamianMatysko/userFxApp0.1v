df6dc2b13e964b6340d39c4a817212a13a28202d
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-06-16 02:46:09 +0200

    Commit Message

--- src/sample/ChatRefresher.java
+++ src/sample/ChatRefresher.java
@@ -12,50 +12,63 @@ import java.io.IOException;
 public class ChatRefresher extends Thread {
     private Thread t;
     boolean stop = true;
+    boolean pauzet = false;
     Text text;
     ServerCommunication serverCommunication;
     public ListView listView;
 
-    public ChatRefresher(Text text,ServerCommunication serverCommunication, ListView listView) {
+    public boolean isPauzet() {
+        return pauzet;
+    }
+
+
+    public ChatRefresher(Text text, ServerCommunication serverCommunication, ListView listView) {
         this.listView = listView;
-        this.text=text;
-        this.serverCommunication=serverCommunication;
+        this.text = text;
+        this.serverCommunication = serverCommunication;
         System.out.println("Vlakno MyThread bolo vytvorene");
     }
 
-    public void run(){
-        System.out.println("Vlakno sa spustilo a bezi");
-
-        while (stop){
-            try {
-                JSONObject response = new JSONObject(serverCommunication.getMessages());
-                ObservableList listLogs = FXCollections.observableArrayList();
-                JSONObject parse;
-
-                for(int i = 1 ; i <= response.length() ; i++){
-                    parse=response.getJSONObject(String.valueOf(i));
-                    listLogs.addAll("FROM: "+parse.getString("from")+" TIME: "+parse.getString("time")+" MESSAGE: "+parse.getString("message"));
-                }
-
-                listView.setItems(listLogs);
-                //text.setText(serverCommunication.getMessages());
-                Thread.sleep(1000);
-            } catch (InterruptedException | IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("Vlakno sa ukoncilo");
+    public void setPauzet(boolean pauzet) {
+        this.pauzet = pauzet;
     }
 
-    public void start(){
+    public void run() {
+        System.out.println("Vlakno sa spustilo a bezi");
+        while(stop)
+            if (!pauzet) {
+                try {
+                    JSONObject response = new JSONObject(serverCommunication.getMessages());
+                    ObservableList listLogs = FXCollections.observableArrayList();
+                    JSONObject parse;
+
+                    for (int i = 1; i <= response.length(); i++) {
+                        parse = response.getJSONObject(String.valueOf(i));
+                        listLogs.addAll("FROM: " + parse.getString("from") + " TIME: " + parse.getString("time") + " MESSAGE: " + parse.getString("message"));
+                    }
+
+                    listView.setItems(listLogs);
+                    //text.setText(serverCommunication.getMessages());
+                    Thread.sleep(1000);
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+
+            }
+        System.out.println("Vlakno sa ukoncilo");
+        }
+
+
+
+    public void start() {
         System.out.println("Start");
-        if(t==null)
-            t=new Thread(this);
+        if (t == null)
+            t = new Thread(this);
         t.start(); // spustil run()
     }
 
-    public void killThread(){
-        stop=false;
+    public void killThread() {
+        stop = false;
     }
 }
--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -13,8 +13,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class HomeController {
 
@@ -28,14 +30,14 @@ public class HomeController {
     public Button deleteMessages;
     public ListView listView;
 
+
+
+    ChatRefresher chatRefresher;
+    private static ServerCommunication serverCommunication = null;
+
     public void setListView(ListView listView) {
         this.listView = listView;
     }
-
-
-
-    ChatRefresher chatRefresher = null;
-    private static ServerCommunication serverCommunication = null;
 
     public void setServerCommunication(ServerCommunication serverCommunication) {
         HomeController.serverCommunication = serverCommunication;
@@ -59,28 +61,46 @@ public class HomeController {
     }
 
     public void showLogs(ActionEvent actionEvent) throws IOException, InterruptedException {
+//   try {
+//       chatRefresher.killThread();//todo ked ide prvi nema co zabit
+//   } catch (Exception e) {
+//
+//   }
 
-        chatRefresher.killThread();//todo ked ide prvi nema co zabit
+     try {
+         chatRefresher.setPauzet(true);
+     } catch (Exception e) {
+
+     }
 
         //textForLogAndMes.setText(serverCommunication.log());
-        System.out.println(serverCommunication.log());
-        String string = serverCommunication.log();
-              JSONObject response = new JSONObject(string);
+        //System.out.println(serverCommunication.log());
+        //List<String> myList = new ArrayList<String>();
+        //myList.se =  serverCommunication.log();
+
+        //String string = serverCommunication.log();
+        //string= string.substring(1, string.length()-1);
+              //JSONObject response = new JSONObject(string);
+
+        JSONObject response = new JSONObject(serverCommunication.log());
         ObservableList listLogs = FXCollections.observableArrayList();
+        JSONObject parse;
 
 
-        for(int i = 0 ; i < response.length() ; i++){
-          listLogs.addAll(response.getJSONObject(String.valueOf(i)));
+        for(int i = 1 ; i <= response.length() ; i++){
+            parse=response.getJSONObject(String.valueOf(i));
+            listLogs.addAll(parse.getString("type").toUpperCase()+" TIME: "+parse.getString("time"));
         }
         listView.setItems(listLogs);
 }
 
-    public void showMessages(ActionEvent actionEvent) throws IOException, InterruptedException {
-        if (chatRefresher == null || chatRefresher.isInterrupted() ) {
+    public void showMessages(ActionEvent actionEvent) throws IOException, InterruptedException {//todo
+
+        if (chatRefresher == null  ) {
             chatRefresher = new ChatRefresher(textForLogAndMes, serverCommunication,listView);
             chatRefresher.start();
         }
-        chatRefresher.interrupt();
+        chatRefresher.setPauzet(false);
     }
 
     public void changePassword(ActionEvent actionEvent) throws IOException {
